35a696bf21cf1327c1e8241ca6cfea6d194cd489
João Vitor Polverari <polarzincomfrio@gmail.com>
2023-06-04 12:42:08 -0300

    added cin.get() to myfirst.cpp

--- ch02/source code/myfirst.cpp
+++ ch02/source code/myfirst.cpp
@@ -9,6 +9,7 @@ int main() // function header
 	cout << "Come up and C++ me some time."; // first message
 	cout << endl; // start a new line
 	cout << "You won't regret it!" << endl; // second message and new line
+	cin.get();
 
 	return 0; // terminate of main()
 
