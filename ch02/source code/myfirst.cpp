d44d840ceee8139c1b19c2a4f7570170ce3e99a2
João Vitor Polverari <polarzincomfrio@gmail.com>
2023-06-04 11:37:21 -0300

    created myfirst.cpp, a source code from the book

---
+++ ch02/source code/myfirst.cpp
@@ -0,0 +1,15 @@
+// my first.cpp -- displays a message
+
+#include <iostream> // a PREPROCESSOR directive
+
+int main() // function header
+{ // start of the function body
+	using namespace std; // make definitions visible
+
+	cout << "Come up and C++ me some time."; // first message
+	cout << endl; // start a new line
+	cout << "You won't regret it!" << endl; // second message and new line
+
+	return 0; // terminate of main()
+
+} // end of the function body
